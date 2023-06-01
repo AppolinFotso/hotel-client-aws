183d30bf04b3fbd7db036de24d2f1692eaceb21a
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-01 21:46:11 +0100

    Added Heading component

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hotel from "./Images/Hotel-logo.png";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img src={Hotel} />
+    </header>
+  );
+};
+
+export default Heading;
