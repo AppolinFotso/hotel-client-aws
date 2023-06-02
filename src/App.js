afe9be26785183d7b6338dc7a642db2650c405a9
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-02 05:01:14 +0100

    Added TouristInfoCards component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import Heading from "./Heading";
 import Bookings from "./Bookings";
 import "./App.css";
+import TouristInfoCards from "./TouristInfoCards";
 
+//
 const App = () => {
   return (
     <div className="App">
-      <Heading/>
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
