9dd8cb96fa2118184aa06e79c9257a188d7f3b50
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-02 07:27:48 +0100

    Added state variable to restaurant component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Bookings from "./Bookings";
 import "./App.css";
 import TouristInfoCards from "./TouristInfoCards";
 import Footer from "./Footer";
-
+import Restaurant from "./Restaurant";
 //
 const contact = [
   "123 Fake Street, London, E1 4UD",
@@ -18,6 +18,7 @@ const App = () => {
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Restaurant />
       <Footer contact={contact} />
     </div>
   );
