53663dd2a141c645b18e652f77123ca891c52dc2
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-06 09:06:06 +0100

    Added onSubmit event on the search form

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import SearchButton from "./SearchButton";
-const Search = () => {
+const Search = (props) => {
   const [searchInput, setSearchInput] = useState("");
   function handleSearchInput(e) {
     setSearchInput(e.target.value);
-    console.log(e.target.value);
+    // console.log(e.target.value);
   }
+  //
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.search(searchInput);
+  }
+  //
   return (
     <div className="search">
       <div className="page-header">
@@ -13,7 +19,7 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSubmit}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
